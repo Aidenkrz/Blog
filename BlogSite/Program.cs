8aac85b554f7ba29d0984ea95f59e6e8873594e8
Aiden <28298836+Aidenkrz@users.noreply.github.com>
2025-04-21 14:16:27 -0500

    Update Program.cs

--- BlogSite/Program.cs
+++ BlogSite/Program.cs
@@ -22,13 +22,6 @@ app.UseForwardedHeaders();
 if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 
-app.Use(async (context, next) => {
-    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-    logger.LogInformation("Request Scheme: {Scheme}, Remote IP: {RemoteIp}", 
-        context.Request.Scheme, context.Connection.RemoteIpAddress);
-    await next();
-});
-
 app.UseRouting();
 
 app.UseAuthorization();
